6cb8f541ea6af84cb5d306535c3ff149fe95c3b8
Dan <5250599@student.gloscol.ac.uk>
2025-07-08 14:33:17 +0100

    divide function added

--- app.test.js
+++ app.test.js
@@ -9,3 +9,9 @@ const multiply = require("./app").multiply;
 test("multiplys two numbers", () => {
   expect(multiply(3, 5)).toBe(15);
 });
+
+const divide = require("./app").divide;
+
+test("divides two numbers", () => {
+  expect(divide(10, 5)).toBe(2);
+});
