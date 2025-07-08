27cd560dc011ddbb18cd6fc1b2176b83446a0d3b
Cud5y <cudsy05real@gmail.com>
2025-07-08 14:29:05 +0100

    moved multiply down?

--- app.test.js
+++ app.test.js
@@ -1,9 +1,10 @@
 const sum = require("./app").sum;
-const multiply = require("./app").multiply;
 
 test("add two numbers", () => {
   expect(sum(3, 5)).toBe(8);
 });
+
+const multiply = require("./app").multiply;
 
 test("multiply two numbers", () =>{
   expect(multiply(3,5)).toBe(15);
