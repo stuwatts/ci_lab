5307e82be83b937bf5193df43aab341c08517cae
StudentMN-lab <5253706@student.gloscol.ac.uk>
2025-07-08 14:42:22 +0100

    add multiply

--- app.test.js
+++ app.test.js
@@ -3,3 +3,8 @@ const sum = require("./app").sum;
 test("add two numbers", () => {
   expect(sum(3, 5)).toBe(8);
 });
+const sum = require("./app").sum;
+
+test("multiply two numbers", () => {
+  expect(sum(3, 5)).toBe(15);
+});
