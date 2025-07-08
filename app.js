e6633ff1e9a413a2448cdae942129acb6852f91f
Cud5y <cudsy05real@gmail.com>
2025-07-08 14:08:28 +0100

    multiply two numbers

--- app.js
+++ app.js
@@ -2,4 +2,9 @@ function sum(a, b) {
   return a + b;
 }
 
+function multiply(a,b) {
+  return a * b;
+}
+
 module.exports = { sum };
+module.exports = {multiply};
--- app.test.js
+++ app.test.js
@@ -1,5 +1,11 @@
+const { multiply } = require("./app");
+
 const sum = require("./app").sum;
 
 test("add two numbers", () => {
   expect(sum(3, 5)).toBe(8);
+});
+
+test("multiply two numbers", () =>{
+  expect(multiply(3,5)).toBe(15);
 });
