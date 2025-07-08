8eca7ca47630fe403970751ddd7aa03b712b67a1
Dan <5250599@student.gloscol.ac.uk>
2025-07-08 14:22:49 +0100

    multiply function added

--- app.js
+++ app.js
@@ -2,4 +2,10 @@ function sum(a, b) {
   return a + b;
 }
 
-module.exports = { sum };
+
+
+function multiply(a, b) {
+  return a * b;
+}
+
+module.exports = { sum, multiply };
--- app.test.js
+++ app.test.js
@@ -3,3 +3,9 @@ const sum = require("./app").sum;
 test("add two numbers", () => {
   expect(sum(3, 5)).toBe(8);
 });
+
+const multiply = require("./app").multiply;
+
+test("multiplys two numbers", () => {
+  expect(multiply(3, 5)).toBe(15);
+});
