19e305a1b3930d515607f82fb3592a2dd4d6081a
StudentMN-lab <5253706@student.gloscol.ac.uk>
2025-07-08 15:03:10 +0100

    multiply

--- app.test.js
+++ app.test.js
@@ -1,10 +1,12 @@
+const { multiply } = require("./app");
+
 const sum = require("./app").sum;
 
 test("add two numbers", () => {
   expect(sum(3, 5)).toBe(8);
 });
-const sum = require("./app").sum;
+const multiply = require("./app").multiply;
 
 test("multiply two numbers", () => {
-  expect(sum(3, 5)).toBe(15);
+  expect(multiply(3, 5)).toBe(15);
 });
