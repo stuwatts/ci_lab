577b4b914503d3dc52ce3125a28e682dedf73c54
Cud5y <cudsy05real@gmail.com>
2025-07-08 14:45:46 +0100

    it may divide

--- app.test.js
+++ app.test.js
@@ -1,17 +1,15 @@
-const sum = require("./app").sum;
-const multiply = require("./app").multiply;
-const divide = require("./app").divide;
+const { sum, multiply, divide } = require("./app");
 
 test("add two numbers", () => {
   expect(sum(3, 5)).toBe(8);
 });
 
-test("multiply two numbers", () =>{
-  expect(multiply(3,5)).toBe(15);
+test("multiply two numbers", () => {
+  expect(multiply(3, 5)).toBe(15);
 });
 
-test("divide two valid numbers", () =>{
-  expect(divide(10,2).toBe(5));
+test("divide two valid numbers", () => {
+  expect(divide(10, 2)).toBe(5);
 });
 
 test("divide two invalid numbers",()=>{
